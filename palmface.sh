dc5d4c695f1796b8e88452219e61d99e01212212
cyx <vsuh@mail.ru>
2025-03-07 17:33:22 +0300

    minor chng.

--- palmface.sh
+++ palmface.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 myDIR=/home/vsuh/my/projects/reminder
+
 cd ${myDIR}
 source ./venv/bin/activate
 pip install --upgrade pip
-pip install -q -r /home/vsuh/my/projects/reminder/requirements.txt
+pip install -q -r requirements.txt
 gunicorn --workers 2 --bind=0.0.0.0:7878 \
 	 --access-logfile /var/log/tg_reminder/access.log \
 	 --error-logfile /var/log/tg_reminder/error.log \
