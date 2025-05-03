4a96840a58f7c38178ab0e2ace7867e7dd4aef4b
cyx <vsuh@mail.ru>
2025-05-03 13:35:01 +0300

    start scripts changed

--- start_rund_service.sh
+++ start_rund_service.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
-source /path/to/your/venv/bin/activate
-python rund.py
+export TELEGRAM_CRON_ROOT=/opt/TELEGRAM-CRON
+
+if [ ! -d ${TELEGRAM_CRON_ROOT} ]; then
+  exit 3
+fi;
+
+cd ${TELEGRAM_CRON_ROOT}
+
+if [ ! -d .venv ]; then
+  python3 -m venv .venv
+fi;
+
+
+source .venv/bin/activate
+
+pip install --no-cache -q -r requirements/rund.txt
+python run.py
+
+
--- start_web_service.sh
+++ start_web_service.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-source /path/to/your/venv/bin/activate
+
+source /opt/TELEGRAM-CRON/.venv/bin/activate
+pip install --no-cache -q -r requirements/web.txt
 gunicorn --workers 3 --bind 0.0.0.0:7878 wsgi:app # Или waitress-serve --port=7878 wsgi:app
+
