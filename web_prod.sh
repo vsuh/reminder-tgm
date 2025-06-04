8d6bbe6d9e6acf2805aa7b5fe23b123ee16907ba
cyx <vsuh@mail.ru>
2025-06-04 20:24:34 +0300

    logs error - 007

--- web_prod.sh
+++ web_prod.sh
@@ -18,7 +18,12 @@ source .venv/bin/activate
 
 pip install --no-cache -q -r requirements/rund.txt
 
-echo "$(date) Test log message" >> /workspaces/cron-tg-docker/log/test.log
+echo "[$(date)] - Test log message" >> /workspaces/cron-tg-docker/log/test.log
+
+# Создаем директорию для логов, если её нет
+mkdir -p ${TLCR_LOGPATH}
+touch ${TLCR_LOGPATH}/gunicorn-access.log ${TLCR_LOGPATH}/gunicorn-error.log
+chown -R appuser:appuser ${TLCR_LOGPATH}
 
 #gunicorn -c gunicorn.conf.py \
 #         --access-logfile log/gunicorn-access.log \
@@ -26,7 +31,14 @@ echo "$(date) Test log message" >> /workspaces/cron-tg-docker/log/test.log
 #         --log-level debug \
 #         -w 2 -b 0.0.0.0:${FLASK_PORT} ${FLASK_APP} 
 
-gunicorn -c gunicorn.conf.py
+#gunicorn -c gunicorn.conf.py
 
-# или для windows
-#waitress-serve --port=${FLASK_PORT} ${FLASK_APP}
+PYTHONUNBUFFERED=1 gunicorn \
+    --access-logfile ${TLCR_LOGPATH}/gunicorn-access.log \
+    --error-logfile ${TLCR_LOGPATH}/gunicorn-error.log \
+    --log-level debug \
+    --capture-output \
+    --enable-stdio-inheritance \
+    -w 2 \
+    -b 0.0.0.0:${FLASK_PORT} \
+    ${FLASK_APP}
