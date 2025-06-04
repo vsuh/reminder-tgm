35de3e64724316903de4522641464ef046f39fe8
cyx <vsuh@mail.ru>
2025-06-04 17:25:53 +0300

    logs error - 001

--- web_prod.sh
+++ web_prod.sh
@@ -18,6 +18,12 @@ source .venv/bin/activate
 
 pip install --no-cache -q -r requirements/rund.txt
 
-gunicorn -w 2 -b 0.0.0.0:${FLASK_PORT} ${FLASK_APP}
+echo $(date)" Test log message" > /workspaces/cron-tg-docker/log/test.log
+
+
+gunicorn --log-level debug \
+     --access-logfile ${TLCR_LOGPATH}/access.log \
+     --error-logfile ${TLCR_LOGPATH}/error.log \
+    -w 2 -b 0.0.0.0:${FLASK_PORT} ${FLASK_APP}
 # или
 #waitress-serve --port=${FLASK_PORT} ${FLASK_APP}
