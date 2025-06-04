757715d5a6afba1c49a87a487f9051a2f976343f
cyx <vsuh@mail.ru>
2025-06-04 18:06:18 +0300

    logs error - 005

--- web_prod.sh
+++ web_prod.sh
@@ -20,12 +20,13 @@ pip install --no-cache -q -r requirements/rund.txt
 
 echo $(date)" Test log message" > /workspaces/cron-tg-docker/log/test.log
 
-gunicorn -c gunicorn.conf.py \
-         --access-logfile log/gunicorn-access.log \
-         --error-logfile log/gunicorn-error.log \
-         --log-level debug \
-         -w 2 -b 0.0.0.0:${FLASK_PORT} ${FLASK_APP} 
+#gunicorn -c gunicorn.conf.py \
+#         --access-logfile log/gunicorn-access.log \
+#         --error-logfile log/gunicorn-error.log \
+#         --log-level debug \
+#         -w 2 -b 0.0.0.0:${FLASK_PORT} ${FLASK_APP} 
 
+gunicorn -c gunicorn.conf.py
 
 # или для windows
 #waitress-serve --port=${FLASK_PORT} ${FLASK_APP}
